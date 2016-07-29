27617ea7ac08d09096bed4f7dbda404fa5ad4ece
Ink1804 <keksman89@gmail.com>
2016-07-29 14:53:54 +0300

    добавил структурную таблицу

--- app/src/main/java/com/example/ink1804/destination/Math/Constants.java
+++ app/src/main/java/com/example/ink1804/destination/Math/Constants.java
@@ -31,4 +31,13 @@ public class Constants {
     public static final String Z_CAPRICORN = "Козерог";
     public static final String Z_AQUARIUS = "Водолей";
     public static final String Z_FISH = "Рыбы";
+
+    public static final String KNIGHT = "Рыцарь";
+    public static final String LEADER = "Вождь";
+    public static final String KING = "Король";
+    public static final String CLOWN = "Шут";
+    public static final String ARISTOCRAT = "Аристократ";
+    public static final String PROFESSOR = "Профессор";
+    public static final String VECTOR = "Вектор";
+
 }
