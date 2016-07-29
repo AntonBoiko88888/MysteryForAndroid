052877df2b9ebe41bf1a2477e7e3c319fae9c2ea
Ink1804 <keksman89@gmail.com>
2016-07-29 13:16:49 +0300

    добавил контанты

---
+++ app/src/main/java/com/example/ink1804/destination/Math/Constants.java
@@ -0,0 +1,34 @@
+package com.example.ink1804.destination.Math;
+
+/**
+ * Created by Ink1804 on 29.07.16.
+ */
+public class Constants {
+    //Годы по китайскому гороскопу Y_
+    public static final String Y_RAT = "Крыса";
+    public static final String Y_SNAKE = "Змея";
+    public static final String Y_BULL = "Бык";
+    public static final String Y_HORSE = "Лошадь";
+    public static final String Y_GOAT = "Коза";
+    public static final String Y_MONKEY = "Обезьяна";
+    public static final String Y_COCK = "Петух";
+    public static final String Y_TIGER = "Тигр";
+    public static final String Y_BOAR = "Кабан";
+    public static final String Y_CAT = "Кот";
+    public static final String Y_DRAGON = "Дракон";
+    public static final String Y_DOG = "Собака";
+
+    //Знаки зодиака Z_
+    public static final String Z_ARIES = "Овен";
+    public static final String Z_TAURUS = "Телец";
+    public static final String Z_TWINS = "Близнецы";
+    public static final String Z_CANCER = "Рак";
+    public static final String Z_LION = "Лев";
+    public static final String Z_VIRGO = "Дева";
+    public static final String Z_LIBRA = "Весы";
+    public static final String Z_SCORPION = "Скорпион";
+    public static final String Z_SAGITTARIUS = "Стрелец";
+    public static final String Z_CAPRICORN = "Козерог";
+    public static final String Z_AQUARIUS = "Водолей";
+    public static final String Z_FISH = "Рыбы";
+}
---
+++ app/src/main/java/com/example/ink1804/destination/Math/Structure_databases.java
@@ -0,0 +1,21 @@
+package com.example.ink1804.destination.Math;
+
+/**
+ * Created by Ink1804 on 29.07.16.
+ */
+public class Structure_databases {
+public String[][] StructureHor = {
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        };
+}
