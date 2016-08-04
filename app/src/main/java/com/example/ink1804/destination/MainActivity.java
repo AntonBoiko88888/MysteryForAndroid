b8bcb8c3f5e67862a46a0f09d58240206d8091d6
Ink1804 <keksman89@gmail.com>
2016-08-04 19:36:40 +0300

    +icons +database class http://developer.alexanderklimov.ru/android/database.php

--- app/src/main/java/com/example/ink1804/destination/MainActivity.java
+++ app/src/main/java/com/example/ink1804/destination/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ink1804.destination;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -8,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
@@ -23,8 +27,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toolbar.setTitle("Destination");
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.app_name, R.string.clown_about);
+
         drawer.setDrawerListener(toggle);
-        toggle.syncState();
+                toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
