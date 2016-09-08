862c4bc11a6802b56791732e20d49aca7d4cb87d
AntonBoiko88888 <anton.bojko.1998@gmail.com>
2016-09-08 18:21:29 +0300

    btn_com_birth

--- app/src/main/java/keen/eye/ink1804/destination/Fragments_Advanced/Compatibility_zodiac_fragment.java
+++ app/src/main/java/keen/eye/ink1804/destination/Fragments_Advanced/Compatibility_zodiac_fragment.java
@@ -22,12 +22,13 @@ import keen.eye.ink1804.destination.R;
 public class Compatibility_zodiac_fragment extends Fragment implements View.OnClickListener {
 
     private View rootView;
+    private MaterialSpinner maleSpinner,femaleSpinner;
+    TextView tv_result;
     private String male,female;
-    private ImageView imv_male,imv_female;
     private int[] zodiacDrawables = {R.drawable.com_zod_oven, R.drawable.com_zod_telec, R.drawable.com_zod_blizneci,
             R.drawable.com_zod_rak, R.drawable.com_zod_lev, R.drawable.com_zod_deva, R.drawable.com_zod_vesi, R.drawable.com_zod_skorpion,
             R.drawable.com_zod_strelec, R.drawable.com_zod_kozerog, R.drawable.com_zod_vodoley, R.drawable.com_zod_ribi};
-    private String[] ZODIAK_NAMES = {"Овен","Телец","Близнецы","Рак","Лев","Дева","Весы","Скорпион","Стрелец","Козерог","Водолей","Рыбы"};
+    private String[] ZODIAK_NAMES ={"Овен","Телец","Близнецы","Рак","Лев","Дева","Весы","Скорпион","Стрелец","Козерог","Водолей","Рыбы"};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.comp_zodiac_fragment_viewpager,container,false);
@@ -38,21 +39,23 @@ public class Compatibility_zodiac_fragment extends Fragment implements View.OnCl
     private void initViews(){
         Button btnGetResult = (Button)rootView.findViewById(R.id.comp_zod_btn_result);
         btnGetResult.setOnClickListener(this);
-        imv_male = (ImageView)rootView.findViewById(R.id.comp_zod_image_male);
-        imv_female = (ImageView)rootView.findViewById(R.id.comp_zod_image_female);
-        MaterialSpinner maleSpinner = (MaterialSpinner) rootView.findViewById(R.id.comp_zod_male_spinner);
-        MaterialSpinner femaleSpinner = (MaterialSpinner) rootView.findViewById(R.id.comp_zod_female_spinner);
+        maleSpinner = (MaterialSpinner) rootView.findViewById(R.id.comp_zod_male_spinner);
+        femaleSpinner = (MaterialSpinner) rootView.findViewById(R.id.comp_zod_female_spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_spinner_item,
                 ZODIAK_NAMES);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         femaleSpinner.setAdapter(adapter);
         maleSpinner.setAdapter(adapter);
+        tv_result = (TextView)rootView.findViewById(R.id.comp_zod_tv_result);
+
 
         maleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                imv_male.setBackgroundResource(zodiacDrawables[i]);
+                ImageView imageView = (ImageView)rootView.findViewById(R.id.comp_zod_image_male);
+                imageView.setBackgroundResource(zodiacDrawables[i]);
                 male = ZODIAK_NAMES[i];
+                tv_result.setText(" ");
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {}
@@ -61,8 +64,10 @@ public class Compatibility_zodiac_fragment extends Fragment implements View.OnCl
         femaleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                imv_female.setBackgroundResource(zodiacDrawables[i]);
+                ImageView imageView = (ImageView)rootView.findViewById(R.id.comp_zod_image_female);
+                imageView.setBackgroundResource(zodiacDrawables[i]);
                 female = ZODIAK_NAMES[i];
+                tv_result.setText(" ");
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {}
@@ -72,7 +77,6 @@ public class Compatibility_zodiac_fragment extends Fragment implements View.OnCl
     @Override
     public void onClick(View view) {
         String key = male + "+" + female;
-        TextView tv_result = (TextView)rootView.findViewById(R.id.comp_zod_tv_result);
         String[] names = getResources().getStringArray(R.array.com_names);
         String[] details = getResources().getStringArray(R.array.com_db);
         int id = -1;
