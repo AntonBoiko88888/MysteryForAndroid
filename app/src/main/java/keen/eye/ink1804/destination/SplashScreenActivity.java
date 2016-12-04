c13e5ce056bc2ff1e5db138363930c9843de785e
AntonBoiko88888 <anton.bojko.1998@gmail.com>
2016-12-04 22:05:09 +0300

    splash_screen norm

--- app/src/main/java/keen/eye/ink1804/destination/SplashScreenActivity.java
+++ app/src/main/java/keen/eye/ink1804/destination/SplashScreenActivity.java
@@ -18,43 +18,10 @@ public class SplashScreenActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-        TextView loading = (TextView)findViewById(R.id.loading);
-        Typeface tf = Typeface.createFromAsset(getResources().getAssets(), "space.otf");
-        loading.setTypeface(tf);
 
-
-        final SharedPreferences mSettings = getSharedPreferences("app_settings", Context.MODE_PRIVATE);
-        if (!mSettings.contains(Constants.APP_PREF_ISREGISTER)) {
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(8000);
-                        startActivity(new Intent(SplashScreenActivity.this, AppIntr.class));
-                        finish();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-        }
-        else {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(3000);
-                        startActivity(new Intent(SplashScreenActivity.this, AppIntr.class));
-                        finish();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }).start();
-        }
+        Intent intent = new Intent(this, AppIntr.class);
+        startActivity(intent);
+        finish();
     }
 }
